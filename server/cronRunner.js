f062c372e9f04869eee2d52c738aee3607bc6049
Luka Krulčić <luka.krulcic305@gmail.com>
2025-08-15 12:46:44 +0200

    Final version v2

--- server/cronRunner.js
+++ server/cronRunner.js
@@ -1,19 +1,12 @@
 const scrapeAllProducts = require("./scrapeCron");
-const cron = require("node-cron");
 
-function logWithTimestamp(message) {
-  const now = new Date().toLocaleString();
-  console.log(`[${now}] ${message}`);
-}
-
-// Pokreni odmah pri startu
 (async () => {
-  logWithTimestamp(" Prvo pokretanje scrapeCron...");
+  const now = new Date().toLocaleString();
+  console.log(`[${now}] Pokrećem scrapeCron...`);
   await scrapeAllProducts();
-})();
-
-// Cron job – u 08:00 i 20:00
-cron.schedule("0 8,20 * * *", async () => {
-  logWithTimestamp("Pokrećem scrapeCron...");
-  await scrapeAllProducts();
+  console.log(`[${new Date().toLocaleString()}] Gotovo.`);
+  process.exit(0); // VAŽNO: zatvori proces nakon jedne runde
+})().catch(err => {
+  console.error(`[${new Date().toLocaleString()}] Greška:`, err);
+  process.exit(1);
 });
